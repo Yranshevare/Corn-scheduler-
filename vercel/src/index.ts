4b249f2eb9ca5326d4194cbd1cc2b8702afe21a6
yadnesh ranshevare <yranshevare2005@gmail.com>
2026-07-19 20:12:14 +0530

    using array of api instead of single ap

--- vercel/src/index.ts
+++ vercel/src/index.ts
@@ -1,17 +1,22 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import 'dotenv/config';
+import "dotenv/config";
 
 const app = new Hono();
 
 app.get("/", async (c) => {
-    const res = await fetch(`${process.env.RENDER_URL}/ping`);
-    console.log(await res.json());
+    const urls = JSON.parse(process.env.RENDER_URLS ?? "[]") as string[];
+
+    const res = await Promise.all(urls.map((url) => fetch(url)));
+    
+    const data = await Promise.all(res.map((response) => response.json()));
+
+    console.log(data);
     return c.text("Hello Hono!");
 });
 
 serve(
-    {   
+    {
         fetch: app.fetch,
         port: 4000,
     },
